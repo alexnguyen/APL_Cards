ff122271db0cf4420bb5bb19604977c030da505c
mguppy <mguppy34@gmail.com>
2025-11-04 06:51:24 -0600

    added all of the child fields

--- script.js
+++ script.js
@@ -35,3 +35,41 @@ function toggleApplicationTypeDivs() {
 
         // Optional: Call the function on page load to set initial state if a radio button is checked by default
         window.onload = toggleApplicationTypeDivs;
+
+function toggleMailingAddressDivs() {
+        const radioButtons = document.querySelectorAll('input[name="option"]');
+        const divs = document.querySelectorAll('div[id^="mailingaddressdiv"]'); // Selects divs starting with "div"
+
+        radioButtons.forEach(radio => {
+            if (radio.checked) {
+            // Hide all divs first
+            divs.forEach(div => {
+                div.style.display = 'none';
+            });
+            // Show the div corresponding to the selected radio button
+            document.getElementById(radio.value).style.display = 'block';
+            }
+        });
+        }
+
+        // Optional: Call the function on page load to set initial state if a radio button is checked by default
+        window.onload = toggleMailingAddressDivs;
+
+function toggleChildMailingAddressDivs() {
+        const radioButtons = document.querySelectorAll('input[name="option"]');
+        const divs = document.querySelectorAll('div[id^="childmailingaddressdiv"]'); // Selects divs starting with "div"
+
+        radioButtons.forEach(radio => {
+            if (radio.checked) {
+            // Hide all divs first
+            divs.forEach(div => {
+                div.style.display = 'none';
+            });
+            // Show the div corresponding to the selected radio button
+            document.getElementById(radio.value).style.display = 'block';
+            }
+        });
+        }
+
+        // Optional: Call the function on page load to set initial state if a radio button is checked by default
+        window.onload = toggleChildMailingAddressDivs;
